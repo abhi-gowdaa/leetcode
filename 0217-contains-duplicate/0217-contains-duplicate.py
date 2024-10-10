dffed7da38a49077506097486b754100e7e5ca38
Abhishek B S <109302022+abhi-gowdaa@users.noreply.github.com>
2024-10-10 14:11:50 +0530

    Time: 419 ms (60.37%), Space: 32 MB (25.53%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.py
+++ 0217-contains-duplicate/0217-contains-duplicate.py
@@ -1,19 +1,42 @@
-class Solution:
-    def containsDuplicate(self, nums: List[int]) -> bool:
-        hashset = set()
-        for v in nums:
-            if  v in hashset:
-                return True
-            hashset.add(v)
-        return False
+# class Solution:
+#     def containsDuplicate(self,nums:List[int])->bool:
+#         maps={}
+#         for num in nums:
+#             if num in maps:
+#                 return True
+#             else:
+#                 maps[num]=1
+#         return False
 
+
+
+
+1
 # class Solution:
 #     def containsDuplicate(self, nums: List[int]) -> bool:
-#         num = set()
-#         count=0
-#         for v in range(len(nums)):
-#             if nums[v] in num:
-#                 return True
-#             num.add(nums[v])
-               
-         
+#         # ll=set()
+#         # for i in range(len(nums)):
+#         #     if nums[i] in ll:
+#         #         return True
+#         #     else:
+#         #         ll.add(nums[i])
+#         # return False
+
+# 2
+# class Solution:
+#     def containsDuplicate(self, nums: List[int]) -> bool:
+#         #return len(nums)!=len(set(nums))
+
+
+
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        st=set()
+        for num in nums:
+            if num in st:
+                return True
+            else:
+                st.add(num)
+        return False
+
+      
