1bfb1b273d8c62141b30e6aca0bda6ecca33b689
Abhishek B S <109302022+abhi-gowdaa@users.noreply.github.com>
2024-10-16 12:18:58 +0530

    Time: 59 ms (63.35%), Space: 17.7 MB (39.19%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -1,12 +1,52 @@
-class Solution:
+# class Solution:
+#     def twoSum(self, nums: List[int], target: int) -> List[int]:
+#         mapping = {
+#             # num: index
+#         }
+#         for i, num in enumerate(nums):
+#             diff = target - num
+#             if diff in mapping:
+#                 return [mapping[diff],i]
+#             mapping[num]=i
+#         return
+
+# class Solution:
+#     def twoSum(self, nums: List[int], target: int) -> List[int]:
+        
+#         for i in range(len(nums)):
+#             for j in range(i+1,len(nums)):
+#                 if(nums[i]+nums[j]==target):
+#                     return [i,j]
+         
+    
+    
+# class Solution:
+#     def twoSum(self, nums: List[int], target: int) -> List[int]:
+#         mapping = {
+#             # num: index
+#         }
+#         for i, num in enumerate(nums):
+#             diff = target - num
+#             if diff in mapping:
+#                 return [mapping[diff],i]
+#             mapping[num]=i
+#         return
+
+# class Solution:
+#     def twoSum(self, nums: List[int], target: int) -> List[int]:
+        
+#         for i in range(len(nums)):
+#             for j in range(i+1,len(nums)):
+#                 if(nums[i]+nums[j]==target):
+#                     return [i,j]
+                
+class Solution:           
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        mapping = {
-            # num: index
-        }
-        for i, num in enumerate(nums):
-            diff = target - num
-            if diff in mapping:
-                return [mapping[diff],i]
-            mapping[num]=i
+        mapp={}
+        for i,num in enumerate(nums):
+            diff=target-num
+            if diff in mapp:
+                return [mapp[diff],i]
+            mapp[num]=i
         return
-            
+         
