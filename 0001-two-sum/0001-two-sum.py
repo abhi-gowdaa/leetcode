1cc1ff31ed26eeb93c4cdd8409dc6a6821f62199
Abhishek B S <109302022+abhi-gowdaa@users.noreply.github.com>
2024-10-16 12:19:46 +0530

    Time: 53 ms (86.62%), Space: 17.8 MB (39.19%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -19,27 +19,7 @@
 #                     return [i,j]
          
     
-    
-# class Solution:
-#     def twoSum(self, nums: List[int], target: int) -> List[int]:
-#         mapping = {
-#             # num: index
-#         }
-#         for i, num in enumerate(nums):
-#             diff = target - num
-#             if diff in mapping:
-#                 return [mapping[diff],i]
-#             mapping[num]=i
-#         return
-
-# class Solution:
-#     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        
-#         for i in range(len(nums)):
-#             for j in range(i+1,len(nums)):
-#                 if(nums[i]+nums[j]==target):
-#                     return [i,j]
-                
+ 
 class Solution:           
     def twoSum(self, nums: List[int], target: int) -> List[int]:
         mapp={}
